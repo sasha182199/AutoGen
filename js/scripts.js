98620234acc94a118b54c3276c286c5661d9550a
sasha182199 <sasha182199@gmail.com>
2019-10-27 18:55:19 +0300

    Header mob commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,25 @@ $(document).ready(function(){
 		$(this).addClass("active");
 	});
 		
-
+	$(".menu_item").click(function(){
+		if($(".menu_item .hide_show").hasClass("active")){
+			$(".menu_item .hide_show").removeClass("active");
+			$(".menu_item .hide_show").slideUp();
+		}else{
+			$(this).find(".hide_show").addClass("active").slideDown();
+		}
+		
+	});
+	$(".hamburger_menu").click(function(){
+		$(".bottom_line_header").slideDown();
+		$(".menu_mob_active").fadeIn().css("display","flex");
+	});
+	$(".menu_mob_active").click(function(){
+		$(".bottom_line_header").slideUp();
+		$(".menu_mob_active").fadeOut().css("display","none");
+	});
+		
+	
+	
 	
 });
